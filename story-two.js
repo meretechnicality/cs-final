6a4bca5bb97431fd58ea89261d87623e33550cc7
meretechnicality <49245675+meretechnicality@users.noreply.github.com>
2019-05-27 21:41:10 -0400

    added stuff
    did the thingy

---
+++ story-two.js
@@ -0,0 +1,15 @@
+function suppliesClicked() {
+  document.getElementById('suppliesHidden').style.display = "block";
+}
+
+function bluecardClicked() {
+  document.getElementById('bluecardHidden').style.display = "block";
+}
+
+function phoneClicked() {
+  document.getElementById('phoneHidden').style.display = "block";
+}
+
+function laptopClicked() {
+  document.getElementById('laptopHidden').style.display = "block";
+}
